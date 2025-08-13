4ead0b0e396a86160d8762de1bcc29cf3ec5f423
Subhra007 <berasubhra2004@gmail.com>
2025-08-13 16:08:38 +0530

    Updated 13-08 at 16:08

--- FrontEnd/src/components/StealthComponents/Why.jsx
+++ FrontEnd/src/components/StealthComponents/Why.jsx
@@ -8,7 +8,7 @@ const Why = () => {
             <div className=" max-w-4xl mx-auto text-center mb-8">
                 <h1 className="text-3xl sm:text-4xl font-bold">
                      Why Do You Need {' '}
-                    <span className="text-[#2ea9ff]">Stealth Mode?</span>?
+                    <span className="text-[#2ea9ff]">Stealth Mode</span>?
                 </h1>
                 <p className="text-sm sm:text-base text-gray-300 mt-4">
                     Because it couldn't have been said better:
@@ -26,7 +26,7 @@ const Why = () => {
             {/* Comparison Cards */}
             <div className="relative z-10 max-w-6xl mx-auto grid grid-cols-1 md:grid-cols-2 gap-6 sm:gap-8">
                 {/* In-House */}
-                <div className="rounded-xl p-5 sm:p-6 shadow-lg backdrop-blur-md bg-[#141f3a] hover:scale-105 transform transition-transform duration-300">
+                <div className="shadow-lg backdrop-blur-md bg-[#141f3a]/60 hover:scale-105 transform transition-transform duration-300 rounded-xl border border-white/10 p-6">
                     <h2 className="text-xl sm:text-2xl font-semibold text-gray-200 text-center mb-4">Doing It In-House</h2>
                     <ul className="space-y-3 text-gray-300 text-sm sm:text-base px-4 sm:px-10">
                         <li><FaArrowRight className="inline mr-2 text-red-600" />Missed Reminders & Deadlines</li>
@@ -38,7 +38,7 @@ const Why = () => {
                 </div>
 
                 {/* GrowthShark */}
-                <div className="rounded-xl p-5 sm:p-6 shadow-lg backdrop-blur-md bg-[#141f3a] hover:scale-105 transform transition-transform duration-300">
+                <div className="shadow-lg backdrop-blur-md bg-[#141f3a]/60 hover:scale-105 transform transition-transform duration-300 rounded-xl border border-white/10 p-6">
                     <h2 className="text-xl sm:text-2xl font-semibold text-[#2ea9ff] text-center mb-4">With GrowthShark</h2>
                     <ul className="space-y-3 text-gray-300 text-sm sm:text-base px-4 sm:px-10">
                         <li><FaArrowRight className="inline mr-2 text-green-600" />Timely Reminders And Delegated Alerts</li>
